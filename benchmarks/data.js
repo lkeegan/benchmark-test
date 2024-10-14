40c6278f1f600b2ae9343208df87290d3891b0d3
github-action-benchmark <github@users.noreply.github.com>
2024-10-14 12:03:54 +0000

    add Benchmark (catch2) benchmark result for c32c38aaff003e8d96dadf8620b1ff191ea2dac8

---
+++ benchmarks/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728907434371,
+  "repoUrl": "https://github.com/lkeegan/benchmark-test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "liam@keegan.ch",
+            "name": "Liam Keegan",
+            "username": "lkeegan"
+          },
+          "committer": {
+            "email": "liam@keegan.ch",
+            "name": "Liam Keegan",
+            "username": "lkeegan"
+          },
+          "distinct": true,
+          "id": "c32c38aaff003e8d96dadf8620b1ff191ea2dac8",
+          "message": "example benchmark setup",
+          "timestamp": "2024-10-14T14:03:13+02:00",
+          "tree_id": "89ccb0ae88446115c0ffff65d915e2bf518bda31",
+          "url": "https://github.com/lkeegan/benchmark-test/commit/c32c38aaff003e8d96dadf8620b1ff191ea2dac8"
+        },
+        "date": 1728907434073,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 111.469,
+            "range": "± 6.63637",
+            "unit": "ns",
+            "extra": "100 samples\n270 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 12.7223,
+            "range": "± 4.5461",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
